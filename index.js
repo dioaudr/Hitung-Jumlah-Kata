76a0d2d05b344d4b2650edee1e46dd12cce45c98
Dio Audia <76801729+dioaudr@users.noreply.github.com>
2021-02-09 10:10:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let kalimat "kasur";
+let kal = 0;
+let kal2 = 0;
+
+for(let i = 0; i < kalimat.length; i++){
+    temp = ''
+if(kalimat[i] == "a"){
+    kal1++
+} else if(kalimat[i] == "e"){
+    kal2++
+}
+}
+
+} if(kal == kal2){
+    console.log(true)
+} else{
+    console.log(false)
+}
